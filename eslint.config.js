34afbcfb88196d02afc723a2809a9f90f5984f86
Kiryl Vasko <kirill.vasko.2003@gmail.com>
2026-05-07 15:58:32 +0300

    Revert "Gch 1/project setup"

--- eslint.config.js
+++ eslint.config.js
@@ -1,74 +1,22 @@
-import js from '@eslint/js';
-import prettier from 'eslint-config-prettier';
-import importPlugin from 'eslint-plugin-import';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import { defineConfig, globalIgnores } from 'eslint/config';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'node_modules']),
-
+  globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-
-    plugins: {
-      import: importPlugin,
-    },
-
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      prettier,
     ],
-
     languageOptions: {
       globals: globals.browser,
     },
-
-    rules: {
-      'no-var': 'warn',
-      'prefer-const': 'warn',
-      'no-console': 'warn',
-      'no-debugger': 'warn',
-
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-non-null-assertion': 'warn',
-
-      'react-hooks/rules-of-hooks': 'warn',
-      'react-hooks/exhaustive-deps': 'warn',
-
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-uses-react': 'off',
-      'react/display-name': 'off',
-
-      'prettier/prettier': 'warn',
-
-      'import/no-unresolved': 'off',
-
-      'import/order': [
-        'warn',
-        {
-          'newlines-between': 'always',
-          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-          pathGroups: [
-            {
-              pattern: '@/**',
-              group: 'internal',
-              position: 'after',
-            },
-          ],
-          pathGroupsExcludedImportTypes: ['builtin'],
-          alphabetize: {
-            order: 'asc',
-            caseInsensitive: true,
-          },
-        },
-      ],
-    },
   },
-]);
+])
