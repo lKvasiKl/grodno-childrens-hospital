283329e554f2f28ca42ef8045f47fbcc6e2d197f
Kirill <kirill.vasko.2003@gmail.com>
2026-05-21 11:46:38 +0300

    feat: add simple spin loader

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,5 +2,6 @@ export { default as ErrorBoundary } from './ErrorBoundary';
 export { default as AppFooter } from './Footer';
 export { default as AppHeader } from './Header';
 export { default as HeroBanner } from './HeroBanner';
+export { default as Loader } from './Loader';
 export { default as NavBar } from './NavBar';
 export { default as SocialLinks } from './SocialLinks';
